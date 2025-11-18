abd43ad6c9b01483d5ce7f46160af7619b037952
A4BHI <manthukafc@gmail.com>
2025-11-18 21:18:39 +0530

    wrote some random shit

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,27 +1,59 @@
 package main
 
-import "fmt"
+type Lexer struct {
+	Source     string
+	currentPos int
+	nextPos    int
+	ch         byte
+}
+
+var Out []string
 
 const (
-	MAKE = "MAKE"
+	MAKE  = "MAKE"
+	EQUAL = "EQUAL"
+	IF    = "IF"
+	PLUS  = "PLUS"
 )
 
 var Tokens = map[string]string{
 	"make": MAKE,
+	"if":   IF,
+	"plus": PLUS,
 }
+var s string
 
-func OutPut(src string) {
-	token, ok := Tokens[src]
+func (l *Lexer) OutPut() {
+	for i := 0; i < len(l.Source); i++ {
+		if l.Source[i] == ' ' {
+			l.currentPos++
+		} else {
+			l.ch = l.Source[i]
+			b := handleOperators(l.ch)
 
-	if ok {
-		fmt.Print(token)
-	} else {
-		fmt.Print("IDENT")
+			if !b {
+				s += string(l.ch)
+
+			}
+
+		}
 	}
 
 }
 
+func handleOperators(op byte) bool {
+	switch op {
+	case '+':
+		Out = append(Out, Tokens[string(op)])
+		return true
+		break
+	default:
+		return false
+	}
+	return true
+}
+
 func main() {
 	// OutPut("make")
-	OutPut("x")
+
 }
