8d358f3b3d443fd03d298dbeeb509e93e2f2ca32
A4BHI <manthukafc@gmail.com>
2025-12-19 22:34:16 +0530

    Learning how parser works

--- src/parser.go
+++ src/parser.go
@@ -4,4 +4,11 @@ type Parser struct {
 	l         *Lexer
 	currToken Token
 	nextToken Token
+	Errors    []string
+}
+
+func (p *Parser) nexttoken() {
+	p.currToken = p.nextToken
+	p.nextToken = p.l.NextToken()
+
 }
