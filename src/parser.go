60ad094a3acd7f3043dd7aff1ed44dbf9c77e279
A4BHI <manthukafc@gmail.com>
2026-01-02 22:05:29 +0530

    this sucks fr

--- src/parser.go
+++ src/parser.go
@@ -97,28 +97,19 @@ func (p *Parser) parseExpression() Expression {
 		return &IntegerLiteral{Value: val}
 	case IDENT:
 		if p.PeekNext.Type == LBRAC {
-			
-			fnc := &FunctionCall{}
-			stmt := p.parseExpression()
-			var exp Expression
-			fnc.Function = stmt
-			p.nextToken()
-			if p.PeekNext.Type == RBRAC {
-				p.nextToken()
 
-			}else{
-				p.nextToken()
-				exp=p.parseExpression()
-			}
-			if p.PeekNext.Type=SEMICOLON{
-				p.nextToken()
-			}
-			return &FunctionCall{Function: stmt, Arguments:exp }
+			fnc := &FunctionCall{}
+
+			return &FunctionCall{Function: stmt, Arguments: exp}
 		} else {
 			return &Identifier{Value: p.CurrToken.Literal}
 		}
 
 	}
+}
+
+func (p *Parser) parseFunctionCall() Expression {
+
 }
 
 func NewParser(l *Lexer) *Parser {
