62933c26462351db2bb7e4f8d5a6a1ec7bb4c528
A4BHI <manthukafc@gmail.com>
2026-01-03 19:21:57 +0530

    tested some code

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-	l := klang.NewLexer("make x = foo(1,2);")
+	l := klang.NewLexer("foo(1,2)")
 	p := klang.NewParser(l)
 	pg := p.ParseProgram()
 	fmt.Println("Errors:", p.Errors)
 	fmt.Println("Program:", pg)
-	ms := pg.Statements[0].(*klang.MakeStatement)
+	ms := pg.Statements[0].(*klang.ExpressionStatement)
 
-	fmt.Println(ms.Name.Value)
-	fmt.Println(ms.Value.(*klang.FunctionCall).Function)
-	fmt.Println(ms.Value.(*klang.FunctionCall).Arguments[0].(*klang.IntegerLiteral).Value)
-	fmt.Println(ms.Value.(*klang.FunctionCall).Arguments[1].(*klang.IntegerLiteral).Value)
+	// fmt.Println(ms.Name.Value)
+	fmt.Println(ms.Expression.(*klang.FunctionCall).Function)
+	fmt.Println(ms.Expression.(*klang.FunctionCall).Arguments[0].(*klang.IntegerLiteral).Value)
+	fmt.Println(ms.Expression.(*klang.FunctionCall).Arguments[1].(*klang.IntegerLiteral).Value)
 
 	for i, stmt := range pg.Statements {
 		fmt.Printf("Statement: %d: %#v\n", i, stmt)
